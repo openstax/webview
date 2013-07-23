9606cea1160d4a64bdfa716f2f29cc02fe194fa6
Derek Kent <dak@dak.org>
2013-07-23 00:06:44 -0400

    Add build script

---
+++ build/build.js
@@ -0,0 +1,31 @@
+({
+    appDir: '../site',
+    baseUrl: 'scripts',
+    dir: '../dist',
+    mainConfigFile: '../site/scripts/main.js',
+    findNestedDependencies: true,
+    removeCombined: true,
+    useStrict: true,
+    keepBuildDir: false,
+    preserveLicenseComments: false,
+    optimize: 'uglify2',
+
+    //stubModules: ['cs'],
+
+    modules: [
+        {
+            name: 'main',
+            excludeShallow: [
+                'css/css-builder',
+                'less/lessc-server',
+                'less/lessc'
+            ],
+            include: [/*'css', */'cs!pages/home/home'],
+            //exclude: ['coffee-script']
+        }
+    ],
+
+    uglify2: {
+        mangle: true
+    }
+})
