0948d34f32f40bcf3c5707b0f838b0552731c056
carolinelane10 <cal7@rice.edu>
2014-09-18 14:35:58 -0500

    taking out declaration of should

--- test/scripts/helpers/handlebars/author.js
+++ test/scripts/helpers/handlebars/author.js
@@ -1,21 +1,22 @@
 describe('handlebar author helper tests', function () {
   'use strict';
-  var author, Handlebars, 
-    should = chai.should();
+  var author, Handlebars;
 
   beforeEach(function (done) {
     this.timeout(10000);
 
-    require(['hbs/handlebars','cs!helpers/handlebars/author'], function () {
+    require(['hbs/handlebars', 'cs!helpers/handlebars/author'], function () {
       Handlebars = arguments[0];
       author = arguments[1];
       done();
     });
   });
-  
-  it('should retrieve the name', function() {
-    var myObj = {author: 'Caroline'};
-	var myList = [myObj];
-	Handlebars.helpers.author(myList, 0, 'author').toString().should.equal('Caroline');
+
+  it('should retrieve the name', function () {
+    var myObj = {
+      author: 'Caroline'
+    };
+    var myList = [myObj];
+    Handlebars.helpers.author(myList, 0, 'author').toString().should.equal('Caroline');
   });
 });
