ec53526667a18b615d2f412aaa6b59bc7c90c931
cburk <cjb6@rice.edu>
2015-05-29 19:31:33 +0000

    test pretty much working

--- test/scripts/modules/media/header/header.js
+++ test/scripts/modules/media/header/header.js
@@ -1,8 +1,7 @@
 describe('media header tests', function () {
   'use strict';
-  // What do we actually need?
   var MediaHeaderView, Content, myHeader, Backbone;
-  //    should = chai.should();
+  // var should = chai.should();
 
   beforeEach(function (done) {
     this.timeout(10000);
@@ -12,26 +11,25 @@ describe('media header tests', function () {
       Content = arguments[1];
       Backbone = arguments[2];
 
-      //This defn ran until the tests at least...
       var cont = new Content();
+      cont.set({
+        'title': 'Book',
+        'currentPage': {
+          'title': 'Chapter'
+        }
+      });
       myHeader = new MediaHeaderView({
         'model': cont
       });
+      // why doesn't this work?  Thought: function doesn't have get built in?
+      // cont.get('currentPage').get('title').should.equal('asdf');
       done();
     });
   });
-  describe('title tests', function () {
-    it('should include the page name in title', function () {
-      myHeader.model.set('title', 'A Scandal in Bohemia');
-      myHeader.model.get('title').should.equal('A Scandal in Bohemia');
-      //works to here
-      //myHeader.model.set('currentPage', '7');
-      //myHeader.model.get('currentPage').should.equal('7');
-
-      myHeader.model.get('currentPage').should.equal(myHeader.getModel());
+  describe('title update test', function () {
+    it('should include the title and page name in title', function () {
       myHeader.updateTitle();
-      myHeader.pageTitle.should.equal('stringers');
-      '1'.should.equal('1');
+      myHeader.pageTitle.should.equal('Book - Chapter');
     });
   });
 });
