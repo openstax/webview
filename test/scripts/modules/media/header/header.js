7eaabeede5bebd5368049bf03e3c8bbe04361d89
cburk <cjb6@rice.edu>
2015-06-04 19:04:27 +0000

    refactored tests again

--- test/scripts/modules/media/header/header.js
+++ test/scripts/modules/media/header/header.js
@@ -1,18 +1,12 @@
 describe('media header tests', function () {
   'use strict';
   var headerView, content, page;
-  var Header, Page, Content, Backbone;
 
   beforeEach(function (done) {
     this.timeout(10000);
 
-    require(['cs!modules/media/header/header', 'cs!models/content', 'cs!models/content', 'backbone'],
-      function () {
-        Header = arguments[0];
-        Content = arguments[1];
-        Page = arguments[2];
-        Backbone = arguments[3];
-
+    require(['cs!modules/media/header/header', 'cs!models/contents/page', 'cs!models/content'],
+      function (Header, Page, Content) {
         content = new Content();
         headerView = new Header({
           'model': content
@@ -22,21 +16,11 @@ describe('media header tests', function () {
       });
   });
   describe('title update test', function () {
-    it('should include the title and page name in title when updateTitle is called', function () {
-      page.set('title', 'Page');
-      content.set('currentPage', page);
-      content.set('title', 'Book');
-      headerView = new Header({
-        'model': content
-      });
-      headerView.updateTitle();
-      headerView.pageTitle.should.equal('Book - Page');
-    });
-    it('should automatically call updateTitle when currentPage changes', function () {
+    it('updateTitle should correctly change the title', function () {
       content.set('title', 'Book');
       page.set('title', 'New Chapter');
       content.set('currentPage', page);
-      headerView.pageTitle.should.equal('Book - New Chapter');
+      headerView.updateTitle().should.equal('Book - New Chapter');
     });
   });
 });
