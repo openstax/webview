0d5720d0257bc945be705d09e1c9da13cf8ce055
cburk <cjb6@rice.edu>
2015-10-05 18:51:12 +0000

    refactoring nav.js test

--- test/scripts/modules/media/nav/nav.js
+++ test/scripts/modules/media/nav/nav.js
@@ -1,24 +1,21 @@
 describe('nav tests', function () {
   'use strict';
-  var MediaNavView, Content;
+  var nav, content;
 
   beforeEach(function (done) {
     this.timeout(10000);
 
-    require(['cs!modules/media/nav/nav', 'cs!models/content'], function () {
-      MediaNavView = arguments[0];
-      Content = arguments[1];
+    require(['cs!modules/media/nav/nav', 'cs!models/content'], function (MediaNavView, ContentModel) {
+      content = new ContentModel();
+      nav = new MediaNavView({
+        'model': content
+      });
       done();
     });
   });
   describe('templateHelpers tests', function () {
     it('should set the rel=next/prev tags', function () {
-      var content = new Content();
-      var myNav = new MediaNavView({
-        'model': content
-      });
-
-      myNav.templateHelpers();
+      nav.templateHelpers();
       document.querySelector('link[rel=\'next\']').getAttribute('href').should.equal('/contents/');
       document.querySelector('link[rel=\'prev\']').getAttribute('href').should.equal('/contents/');
       document.querySelectorAll('link[rel=\'next\']').length.should.equal(1);
