1d920b6c6b48b1d712df202af7610a65f0454bd6
cburk <cjb6@rice.edu>
2015-10-05 19:16:02 +0000

    testing requires in nav test

--- test/scripts/modules/media/nav/nav.js
+++ test/scripts/modules/media/nav/nav.js
@@ -1,25 +1,16 @@
 describe('nav tests', function () {
   'use strict';
-  var nav, content;
+  var page;
 
   beforeEach(function (done) {
-    this.timeout(10000);
-
-    require(['cs!modules/media/nav/nav', 'cs!models/content'], function (MediaNavView, ContentModel) {
-      content = new ContentModel();
-      nav = new MediaNavView({
-        'model': content
-      });
+    require(['cs!models/contents/page'], function (Page) {
+      page = new Page();
       done();
     });
   });
   describe('templateHelpers tests', function () {
     it('should set the rel=next/prev tags', function () {
-      nav.templateHelpers();
-      document.querySelector('link[rel=\'next\']').getAttribute('href').should.equal('/contents/');
-      document.querySelector('link[rel=\'prev\']').getAttribute('href').should.equal('/contents/');
-      document.querySelectorAll('link[rel=\'next\']').length.should.equal(1);
-      document.querySelectorAll('link[rel=\'prev\']').length.should.equal(1);
+      page.get('content').should.not.equal('');
     });
   });
 });
