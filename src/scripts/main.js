894ba20f2e7a94729bae592f5e02d32b25b4788a
pandafulmanda <pandafulmanda@gmail.com>
2017-01-25 15:47:01 -0600

    lint and beautify, appease the travis gods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,8 @@
   'use strict';
 
   // Load the config
-  require(['config', 'settings'], function(config, settings){
-    require(['settings/config', 'cs!loader', 'cs!helpers/underscore.deepExtend'], function(configureFor, loader, _){
+  require(['config', 'settings'], function (config, settings) {
+    require(['settings/config', 'cs!loader', 'cs!helpers/underscore.deepExtend'], function (configureFor, loader, _) {
       // some paths and shims need to be configured based on settings.
       configureFor(settings, _);
       // Load the application after the config
