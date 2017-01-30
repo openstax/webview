bae606a3aecef95753b449eb38fcce8e65ae9f2d
pandafulmanda <pandafulmanda@gmail.com>
2017-01-30 15:33:42 -0600

    linting and use promise to run initializing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,18 @@
 
   // Load the config
   require(['config', 'settings'], function (config, settings) {
-    // jshint maxparams: 4
-    require(['settings/feature-config', 'cs!loader', 'cs!helpers/underscore.deepExtend', 'jquery'],
-      function (configureFor, loader, _, $) {
+    // jshint maxparams: 5
+    require([
+        'settings/feature-config', 'settings/concept-coach-enabled-config',
+        'cs!loader', 'cs!helpers/underscore.deepExtend', 'jquery'
+      ],
+      function (configureFor, makeCoachEnabledConfig, loader, _, $) {
         // some paths and shims need to be configured based on settings.
-        configureFor(settings, _, $, function () {        
-          // Load the application after the config
-          loader.init();
-        });
+        configureFor(settings, makeCoachEnabledConfig, _, $)
+          .always(function () {
+            // Load the application after the config
+            loader.init();
+          });
       }
     );
   });
--- src/scripts/settings/feature-config.js
+++ src/scripts/settings/feature-config.js
@@ -2,47 +2,42 @@
   'use strict';
 
   define(function () {
-    return function (settings, _, $, callback) {
-      require(['settings/concept-coach-enabled-config'], function (makeCoachEnabledConfig) {
-
-        // config per feature as needed. The key should match the feature string in
-        // the features' array, and then you can modify the config for either
-        // `enable`d or 'disable'd features.
-        var features = {
-          conceptCoach: {
-            enable: makeCoachEnabledConfig(settings, _, $)
-          }
-        };
-
-        function handleOptions(options) {
-          if (_.isFunction(options)) {
-            return options;
-          } else if (_.isObject(options)) {
-            return _.clone(options) || {};
-          }
+    // jshint maxparams: 4
+    return function (settings, makeCoachEnabledConfig, _, $) {
+      // config per feature as needed. The key should match the feature string in
+      // the features' array, and then you can modify the config for either
+      // `enable`d or 'disable'd features.
+      var features = {
+        conceptCoach: {
+          enable: makeCoachEnabledConfig(settings, _, $)
         }
+      };
 
-        function configureForFeatures(features) {
-          var featureConfigs = _.map(features, function (featureOptions, feature) {
-            if (_.contains(settings.features, feature)) {
-              return handleOptions(featureOptions.enable);
-            } else {
-              return handleOptions(featureOptions.disable);
-            }
+      function handleOptions(options) {
+        if (_.isFunction(options)) {
+          return options;
+        } else if (_.isObject(options)) {
+          return _.clone(options) || {};
+        }
+      }
+
+      function configureForFeatures(features) {
+        var featureConfigs = _.map(features, function (featureOptions, feature) {
+          if (_.contains(settings.features, feature)) {
+            return handleOptions(featureOptions.enable);
+          } else {
+            return handleOptions(featureOptions.disable);
+          }
+        });
+
+        return $.when.apply($, featureConfigs)
+          .done(function () {
+            var featureConfig = _.deepExtend.apply(_, arguments);
+            require.config(featureConfig);
           });
+      }
 
-          $.when.apply($, featureConfigs)
-            .done(function () {
-              var featureConfig = _.deepExtend.apply(_, arguments);
-              require.config(featureConfig);
-            }).then(function () {
-              callback();
-            });
-        }
-
-        configureForFeatures(features);
-
-      });
+      return configureForFeatures(features);
     };
   });
 
