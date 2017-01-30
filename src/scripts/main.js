9928a488afa2668e08aa6c2a5782669479a1c090
pandafulmanda <pandafulmanda@gmail.com>
2017-01-30 14:38:42 -0600

    silly require cant handle var and jshint linelen

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,15 @@
 
   // Load the config
   require(['config', 'settings'], function (config, settings) {
-    var dependencies = ['settings/feature-config', 'cs!loader', 'cs!helpers/underscore.deepExtend', 'jquery'];
-
     // jshint maxparams: 4
-    require(dependencies, function (configureFor, loader, _, $) {
-      // some paths and shims need to be configured based on settings.
-      configureFor(settings, _, $);
-      // Load the application after the config
-      loader.init();
-    });
+    require(['settings/feature-config', 'cs!loader', 'cs!helpers/underscore.deepExtend', 'jquery'],
+      function (configureFor, loader, _, $) {
+        // some paths and shims need to be configured based on settings.
+        configureFor(settings, _, $);
+        // Load the application after the config
+        loader.init();
+      }
+    );
   });
 
 })();
