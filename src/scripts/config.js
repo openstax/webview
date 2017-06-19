317ec7c08ed323523964203626f2d26792996f5e
sisiz <zimmerman.sisi@gmail.com>
2017-06-19 11:11:47 -0500

    trying to change path to select2

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -50,7 +50,7 @@
       bootstrapTransition: '../../bower_components/bootstrap/js/transition',
 
       // # Select2 multiselect widget
-      select2: '../../bower_components/select2/select2',
+      select2: '../../bower_components/select2/dist/js/select2',
 
       // # CoffeeScript
       cs: '../../bower_components/require-cs/cs',
