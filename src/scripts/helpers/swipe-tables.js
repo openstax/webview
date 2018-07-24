958d4b475cac180f9442f88f6dc5c183c51503bf
Piotr Kozlowski <pior@obrandowani.pl>
2018-07-24 17:33:04 +0200

    fix some travis errors

--- src/scripts/helpers/swipe-tables.js
+++ src/scripts/helpers/swipe-tables.js
@@ -1,46 +1,50 @@
-if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  let tablesToSwipe = Array.prototype.filter.call(document.getElementsByClassName('os-table'), function(el){
-    if (el && (el.offsetWidth < el.getElementsByTagName('table')[0].offsetWidth)) {
-      return el
-    }
-  })
-  
-  if (tablesToSwipe.length > 0) {
-    tablesToSwipe.forEach(function(el){
-      el.classList.add('swipe-table')
-      el.addEventListener('mousedown', function(){
-        startSwiping(el)
-      })
-      el.addEventListener('mouseup', function(){
-        stopSwiping(el)
-      })
-      el.addEventListener('mouseleave', function(){
-        stopSwiping(el)
-      })
-    })
-  }
-  
-  let tableLeftPos, startSwipingPos, swipeRange
-  
+(function () {
+  'use strict';
+
+  var tableLeftPos, startSwipingPos, swipeRange;
+    
   function handleSwipe (target) {
-    let mouseX = window.event.clientX
-    let swipeDistance = startSwipingPos - mouseX
+    var mouseX = window.event.clientX;
+    var swipeDistance = startSwipingPos - mouseX;
     if (swipeDistance <= swipeRange && swipeDistance >= (-1 * swipeRange)) {
-      target.scrollLeft += swipeDistance
+      target.scrollLeft += swipeDistance;
     }
   }
   
   function startSwiping (target) {
-    offsets = target.getBoundingClientRect()
-    tableLeftPos = offsets.left
-    startSwipingPos = window.event.clientX
-    swipeRange = target.getElementsByTagName('table')[0].offsetWidth - target.offsetWidth
-    target.onmousemove = function(e) {
-      handleSwipe(target)
-    }
+    var offsets = target.getBoundingClientRect();
+    tableLeftPos = offsets.left;
+    startSwipingPos = window.event.clientX;
+    swipeRange = target.getElementsByTagName('table')[0].offsetWidth - target.offsetWidth;
+    target.onmousemove = function() {
+      handleSwipe(target);
+    };
   }
   
   function stopSwiping (target) {
-    target.onmousemove = function(){}
+    target.onmousemove = null;
   }
-}
+
+  if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    var tablesToSwipe = Array.prototype.filter.call(document.getElementsByClassName('os-table'), function(el){
+      if (el && (el.offsetWidth < el.getElementsByTagName('table')[0].offsetWidth)) {
+        return el;
+      }
+    });
+    
+    if (tablesToSwipe.length > 0) {
+      tablesToSwipe.forEach(function(el){
+        el.classList.add('swipe-table');
+        el.addEventListener('mousedown', function(){
+          startSwiping(el);
+        });
+        el.addEventListener('mouseup', function(){
+          stopSwiping(el);
+        });
+        el.addEventListener('mouseleave', function(){
+          stopSwiping(el);
+        });
+      });
+    }
+  }
+})();
