744e7412d0ffd68fae4d93d0eef218c1a1cdbb8b
tom <tom.g.woodward@gmail.com>
2018-09-06 14:33:08 -0400

    wip - adding tests for donate

---
+++ test/scripts/modules/header/header.js
@@ -0,0 +1,20 @@
+describe('header tests', function () {
+  'use strict';
+  var headerView;
+
+  beforeEach(function (done) {
+    this.timeout(10000);
+
+    require(['cs!modules/header/header'],
+      function (Header) {
+        headerView = new Header();
+        done();
+      });
+  });
+
+  describe('donate link', function () {
+    it('should go to openstax', function () {
+      'asdf'.should.equal('qwer');
+    });
+  });
+});
