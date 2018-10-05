1c901d8e7f570767c662b92500c956c6838ff361
Michael Mulich <michael.mulich@gmail.com>
2018-10-05 12:17:54 -0700

    Adjust nginx config generator for dev vs prod
    This splits the dev configuration from that used by the 'prod' configuration.
    This effectively allows the dev and prod versions to work in the browser.
    And as far as I can tell, it allows the tests to run.

--- .dockerfiles/configure-nginx-from-env.sh
+++ .dockerfiles/configure-nginx-from-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set defaults
-
+ENVIRONMENT=${ENVIRONMENT:-dev}
 ARCHIVE_HOST=${ARCHIVE_HOST:-archive.cnx.org}
 ARCHIVE_PORT=${ARCHIVE_PORT:-80}
 OPENSTAX_HOST=${OPENSTAX_HOST:-openstax.org}
@@ -17,6 +17,23 @@ esac
 
 ROOT=${1:-/code/dist}
 
+# Define environment specific configuration
+case "${ENVIRONMENT}" in
+    'dev')
+        DEV_CONFIG=$(echo """
+  location ~ ^.*/bower_components/(.*)$ {
+      alias ${ROOT}/../bower_components/\$1;
+  }
+
+  location ~ ^.*/data/(.*) {
+      alias ${ROOT}/test/data/\$1;
+  }""")
+        ;;
+    *)
+        DEV_CONFIG=""
+        ;;
+esac
+
 # Output the nginx config to stdout
 echo """\
 server {
@@ -29,14 +46,7 @@ server {
   location ~ ^/resources/.+ {
       proxy_pass ${ARCHIVE_PROTOCOL}://${ARCHIVE_HOST}:${ARCHIVE_PORT};
   }
-
-  location ~ ^.*/bower_components/(.*)$ {
-      alias bower_components/\$1;
-  }
-
-  location ~ ^.*/data/(.*) {
-      alias test/data/\$1;
-  }
+${DEV_CONFIG}
 
   location ~ ^.*/(scripts|styles|images)/(.*) {
       try_files \$uri \$uri/ /\$1/\$2;
